d43546160198d64617f5b740969c9e2ddf424bb0
Franklin Souza <83979476+ffraanks@users.noreply.github.com>
2022-02-03 19:16:50 -0300

    Update qemu_install.sh

--- scripts/qemu_install.sh
+++ scripts/qemu_install.sh
@@ -7,6 +7,7 @@
 
 downqemu(){
   while true ; do
+    mkdir $HOME/.QemuVM
 
   clear
   printf "Deseja baixar o qemu?\n[1] - Sim\n[2] - Não\n[3] - Sair\n\n"
@@ -55,29 +56,27 @@ qemuCLI(){
 ╚██████╔╝███████╗██║ ╚═╝ ██║╚██████╔╝    ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗
  ╚══▀▀═╝ ╚══════╝╚═╝     ╚═╝ ╚═════╝     ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝
                                                                                                \n\n"
-    printf "Escolha uma das oções baixo:\n\n[1] - Criar VM\n[2] - Remover VM\n[3] - Executar VM\n[4] - Baixar ISO\n[5] - Voltar\n[6] - Sair\n\n"
+    printf "Escolha uma das oções baixo:\n\n[1] - Criar VM\n[2] - Remover VM\n[3] - Executar VM\n[4] - Baixar ISO\n[5] - Voltar\n[6] - Listar VMs\n[7] - Sair\n\n"
     read CHOICE_OPTION
     if [ $CHOICE_OPTION == '1' ] || [ $CHOICE_OPTION == '01' ] ; then
       clear
       cd $HOME/.QemuVM
       printf "Coloque um nome para seu HD (EVITE DE COLOCAR ESPAÇOS E TRAÇOS)\n\n"
       read HDNAME
-      printf "\n\nDefina o tamanho do seu HD (EX: 30G - Para 30 Gigas)\n\n"
+      clear && printf "Defina o tamanho do seu HD (EX: 30G - Para 30 Gigas)\n\n"
       read HDSIZE
       qemu-img create -f qcow2 "$HDNAME.qcow2" "$HDSIZE"
       read -p 'Agora vamos configurar o qemu PRESS ENTER TO CONTINUE...' && clear
       printf "Defina o tamanho da memoria ram (EX: 2G - Para 2 Gigas)\n\n"
       read RAMSIZE
-      printf "\n\nDefina quantos nucleos quer para a sua VM (EX: 2 - Para 2 Nucleos)\n\n"
+      clear && printf "Defina quantos nucleos quer para a sua VM (EX: 2 - Para 2 Nucleos)\n\n"
       read NUCLEOS
-      printf "\n\nDefina um nome para a sua VM (EX: Arch Linux)\n\n"
+      clear && printf "Defina um nome para a sua VM (EX: Arch Linux)\n\n"
       read NAMEISO
-      read -p 'Configuração finalizada, aguarde...' && clear
+      read -p 'Configuração finalizada, PRESS ENTER TO CONTINUE...' && clear
       printf "Escolha a .iso que queira executar (Recomendado: Copiar e colar o nome)\n\n" && ls *.iso
-      read ISONAME
+      printf "\n" && read ISONAME
       uefi_bios
-      #cd $HOME/.QemuVM && clear && printf "Rodando qemu..." && qemu-system-x86_64 --enable-kvm -m "$RAMSIZE" -smp "$NUCLEOS" -name '$NAMEISO' -boot d -hda "$HDNAME.qcow2" -cdrom "$ISONAME"
-      
     
     elif [ $CHOICE_OPTION == '2' ] || [ $CHOICE_OPTION == '02' ] ; then
       del
@@ -85,31 +84,28 @@ qemuCLI(){
     elif [ $CHOICE_OPTION == '3' ] || [ $CHOICE_OPTION == '03' ] ; then
       clear
       cd $HOME/.QemuVM
-      ls *.qcow2
-      #printf "\n\nDefina o tamanho da sua Memoria RAM (EX: 2G - Para 2 Gigas)\n\n"
-      #read RAMSIZE1
-      #printf "\n\nDefina quantos nucleos sua VM vai usar (EX: 2 - Para 2 Nucleos)\n\n"
-      #read NUCLEOS1
-      printf "\n\nDefina o nome para sua VM (EX: Arch Linux)\n\n"
+      printf "Defina o nome para sua VM (EX: Arch Linux)\n\n"
       read NAMEISO1
-      #printf "\n\nCopia e cole o arquivo .iso\n\n"
-      #read ISONAME1
-      printf "\n\nCopie e cole o nome do HD.qcow2\n\n"
-      read HDNAME1
+      clear && printf "Copie e cole o nome do HD.qcow2\n\n" && ls *.qcow2
+      printf "\n" && read HDNAME1
       clear && printf "Rodando qemu..." && qemu-system-x86_64 --enable-kvm -name '$NAMEISO1' -drive format=qcow2,file="$HDNAME1"
 
     elif [ $CHOICE_OPTION == '4' ] || [ $CHOICE_OPTION == '04' ] ; then
       clear
       cd $HOME/.QemuVM
       printf "Cole o download do link da iso:\n\n"
-      read DOWNLOADISO
+      printf "\n" && read DOWNLOADISO
       wget -c "$DOWNLOADISO" && read -p 'PRESS ENTER TO CONTINUE...' && continue
 
     elif [ $CHOICE_OPTION == '5' ] || [ $CHOICE_OPTION == '05' ] ; then
     downqemu
 
     elif [ $CHOICE_OPTION == '6' ] || [ $CHOICE_OPTION == '06' ] ; then
-      clear && exit
+      cd $HOME/.QemuVM && ls *.qcow2
+      read -p 'PRESS ENTER TO CONTINUE...' && continue
+
+    elif [ $CHOICE_OPTION == '7' ] || [ $CHOICE_OPTION == '07' ] ; then
+      clear && exit 0
 
     else
       clear && read -p 'Opção incorreta, PRESS ENTER TO CONTINUE...' && continue
@@ -121,11 +117,10 @@ uefi_bios(){
 read UEFI_BIOS
 
 if [ $UEFI_BIOS == '1' ] || [ $UEFI_BIOS == '01' ] ; then
-   #printf "Em desenvolvimento...\n"
-   #read -p 'PRESS ENTER TO CONTINUE...'
-   #qemuCLI
+   printf "Em desenvolvimento...\n"
+   read -p 'PRESS ENTER TO CONTINUE...' && qemuCLI
    #cd $HOME/.QemuVM && clear && printf "Rodando qemu..." && qemu-system-x86_64 --enable-kvm -m "$RAMSIZE" -smp "$NUCLEOS" -name '$NAMEISO' -bios /usr/share/ovmf/OVMF.fd -hda "$HDNAME.qcow2" -cdrom "$ISONAME"
-   cd $HOME/.QemuVM && clear && printf "Rodando qemu..." && qemu-system-x86_64 --enable-kvm -m "$RAMSIZE" -smp "$NUCLEOS" -name '$NAMEISO' -boot uefi -hda "$HDNAME.qcow2" -cdrom "$ISONAME"
+   #cd $HOME/.QemuVM && clear && printf "Rodando qemu..." && qemu-system-x86_64 --enable-kvm -m "$RAMSIZE" -smp "$NUCLEOS" -name '$NAMEISO' -boot uefi -hda "$HDNAME.qcow2" -cdrom "$ISONAME"
 
 elif [ $UEFI_BIOS == '2' ] || [ $UEFI_BIOS == '02' ] ; then
    cd $HOME/.QemuVM && clear && printf "Rodando qemu..." && qemu-system-x86_64 --enable-kvm -m "$RAMSIZE" -smp "$NUCLEOS" -name '$NAMEISO' -boot d -hda "$HDNAME.qcow2" -cdrom "$ISONAME"
@@ -143,12 +138,12 @@ read DEL
 
 if [ $DEL == '1' ] || [ $DEL == '01' ] ; then
   ls *.iso
-  read DELETEISO
+  printf "\n" && read DELETEISO
   rm -rf "$DELETEISO"
 
 elif [ $DEL == '2' ] || [ $DEL == '02' ] ; then
   ls *.qcow2
-  read DELHD
+  printf "\n" && read DELHD
   rm -rf "$DELHD"
       
 else
