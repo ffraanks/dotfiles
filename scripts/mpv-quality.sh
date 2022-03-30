f14561fca43e5e8d5c2a75a5d980c290a2d6c6c8
Franklin Souza <83979476+ffraanks@users.noreply.github.com>
2022-03-30 06:57:18 -0300

    Delete mpv-quality.sh

--- scripts/mpv-quality.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-clear && mkdir $HOME/.config/mpv
-clear && cd $HOME/.config/mpv && mkdir scripts && mkdir scripts-opts && cd
-clear && cd $HOME/.config/mpv/scripts && wget -c "https://github.com/ffraanks/dotfiles/raw/master/youtube-quality.lua" && clear
-cd $HOME/.config/mpv/scripts-opts && wget -c "https://github.com/ffraanks/dotfiles/raw/master/youtube-quality.conf" && cd
-clear && exit 0
-
