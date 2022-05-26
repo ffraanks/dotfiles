566f74bf0d876b3a3ccd7eb748e777daacfd182f
Franklin Souza <83979476+ffraanks@users.noreply.github.com>
2022-05-26 20:25:06 -0300

    Update skyrim_config.sh

--- scripts/skyrim_config.sh
+++ scripts/skyrim_config.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+# Script para instalar a tradução no Skyrim Special Edition Steam
+
 clear && [ ! `command -v unzip` ] && echo "unzip não instalado, por favor faça a instalação" && exit 1
 
 clear
 cd $HOME/Downloads && unzip Traducao-Skyrim-Special-Edition-24159-2-9-1636894034.zip && rm -rf "Traducao-Skyrim-Special-Edition-24159-2-9-1636894034.zip"
-cd Data && cp -r Interface Strings $HOME/.steam/steam/steamapps/common/Skyrim Special Edition/Data
-cd $HOME
-clear && exit 0
-
+cd $HOME/Downloads/Data && cp -r * ~/.steam/steam/steamapps/common/Skyrim\ Special\ Edition/Data/
+cd $HOME/Downloads && rm -rf Data && cd %HOME
+clear && read -p 'PRESSIONE ENTER PARA SAIR...' && clear && exit 0
